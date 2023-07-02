0fefc76a9540cdbe228d3e0c4fc61ceee774453b
Jeancar Sanchez <jeancar.anime.514@gmail.com>
2023-07-02 09:53:49 -0400

    listo

--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -9,7 +9,7 @@ import ButtomWhat from '@/components/buttomWhat/buttomWhat';
 function Contacto(){
     const navStyle = {
         fondo : {background:"linear-gradient(white,rgb(245, 245, 255))"},
-        letter : {color: "#7c0000"},
+        letter : {color: "black"},
         logo : {color: "blackRed"},
       }
     return(
